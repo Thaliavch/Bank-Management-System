5bf6358264618e9fd92708167b372541ffccaed1
Thaliavch <71163693+Thaliavch@users.noreply.github.com>
2022-11-27 15:26:10 -0500

    Update defs.h

--- defs.h
+++ defs.h
@@ -1,17 +1,24 @@
-//
-// Created by Thalia on 11/26/2022.
-//
-
+/* --------------------------------------------------------
+ *    File: defs.h
+ *  Author: Thalia Valle Chavez | David Bentazos | Sahbah Gonzales
+ *   Class: COP 3003, CRN: 13969
+ * Purpose: Bank Management System elements' definitions.
+ * -------------------------------------------------------- */
 #ifndef MAIN_CPP_DEFS_H
 #define MAIN_CPP_DEFS_H
+
 
 // Constants declaration
 const int MIN_PASSWORD = 8;
 const int MAX_PASSWORD = 50;
 const long long int FIRST_ACCOUNT_NUMBER = 10000;
 const int MAX_PASSWORD_VERIFICATION_ATTEMPTS = 8;
+const int SLEEP_TIME = 2;
+const int MENU_SLEEP_TIME = 0.5;
 
-// enums for user inputs, used later in the switch statements.
+
+
+// Enums for user inputs, used later in the switch statements.
 enum UserInput{
     CreateAccount = 1,
     EditAccount,
