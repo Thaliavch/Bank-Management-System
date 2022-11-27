2e9b929bc86d3664c0a45117930df3b6e569d191
Thaliavch <71163693+Thaliavch@users.noreply.github.com>
2022-11-26 19:40:56 -0500

    Add files via upload

---
+++ defs.h
@@ -0,0 +1,26 @@
+//
+// Created by Thalia on 11/26/2022.
+//
+
+#ifndef MAIN_CPP_DEFS_H
+#define MAIN_CPP_DEFS_H
+
+// Constants declaration
+const int MIN_PASSWORD = 8;
+const long long int FIRST_ACCOUNT_NUMBER = 10000;
+const int MAX_PASSWORD_VERIFICATION_ATTEMPTS = 8;
+
+// enums for user inputs, used later in the switch statements.
+enum UserInput{
+    CreateAccount = 1,
+    EditAccount,
+    CheckBalance,
+    AccountInformation,
+    DepositFunds,
+    WithdrawFunds,
+    CloseAccount,
+    ExitProgram
+
+};
+
+#endif //MAIN_CPP_DEFS_H
